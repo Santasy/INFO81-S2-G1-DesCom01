e2d22e78ce1fd6f0fc66d02728d5c8dc30ca952d
Louwz777 <sebita.burgos.m@outlook.com>
2025-11-05 21:53:12 -0300

    modified:   ppdc_timed_generator/generadores/__init__.py 	new file:   ppdc_timed_generator/generadores/generador_constante.py

--- ppdc_timed_generator/generadores/__init__.py
+++ ppdc_timed_generator/generadores/__init__.py
@@ -1,6 +1,8 @@
 from .generador_uniforme import GeneradorUniforme
+from .generador_constante import GeneradorConstante
 
 __all__ = [
     # Core classes
     "GeneradorUniforme",
+    "GeneradorConstante",
 ]
